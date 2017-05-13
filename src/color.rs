0b8e511ee16ec7585d8480b1e6e52325151af364
Brad Svercl <bradsvercl@gmail.com>
2017-05-13 17:46:19 -0500

    Refactor how colors are made

--- src/color.rs
+++ src/color.rs
@@ -1,13 +1,16 @@
-use graphics::types::Color;
+use graphics::types::{Color, ColorComponent};
 
 macro_rules! make_color {
     ($r:expr, $g:expr, $b:expr, $a:expr) => (
-        [$r / 255.0, $g / 255.0, $b / 255.0, $a / 255.0]
+        [$r as ColorComponent / 255.0,
+         $g as ColorComponent / 255.0,
+         $b as ColorComponent / 255.0,
+         $a as ColorComponent / 255.0]
     );
 
     ($r:expr, $g:expr, $b:expr) => (
-        make_color!($r, $g, $b, 255.0);
+        make_color!($r, $g, $b, 255);
     );
 }
 
-pub const CORNFLOWER_BLUE: Color = make_color!(100.0, 149.0, 237.0);
+pub const CORNFLOWER_BLUE: Color = make_color!(100, 149, 237);
